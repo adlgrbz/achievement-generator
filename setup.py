a15b03f2a6f62d1e712f626280f4e80e9183d67a
adlgrbz <adlgrbz000@gmail.com>
2019-09-15 13:50:56 +0300

    bug fix

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
+from setuptools import setup
 
+setup(
+    packages=['mcag'],
+    name = 'mcag',
+    version = '0.1.0',
+    author='Adil Gurbuz',
+    author_email='adlgrbz@protonmail.com',
+    url='https://github.com/adlgrbz/achievement-generator',
+    install_requires=[
+        'pillow==6.1.0',
+    ],
+    entry_points='''
+        [console_scripts]
+        mcag=achievement-generator.mcag:main
+    ''',
+)
